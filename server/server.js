f58f7511565a6113f7065c8ffc53cf8042f01ddc
ksami <ksami.ihide@gmail.com>
2015-01-24 20:28:17 +0800

    setinterval for tweet requests

--- server/server.js
+++ server/server.js
@@ -19,30 +19,46 @@ if (Meteor.isServer) {
   var speakeasy = Meteor.npmRequire('speakeasy-nlp');
 
   // async callbacks to be wrapped in Meteor.bindEnvironment()
-  twitter.get('search/tweets', { q: 'snsd since:2011-11-11 lang:en', count: 3 }, Meteor.bindEnvironment(
-    function(err, data, response) {
-      console.log(data);
+  
+  setInterval(Meteor.bindEnvironment(function(){
+    twitter.get('search/tweets', { q: 'snsd since:2011-11-11 lang:en', count: 20 }, Meteor.bindEnvironment(
+      function(err, data, response) {
+        console.log(data);
 
-      for (var i = data.statuses.length - 1; i >= 0; i--) {
-        var status = data.statuses[i];
+        for (var i = data.statuses.length - 1; i >= 0; i--) {
+          var status = data.statuses[i];
 
-        var sentiment = speakeasy.sentiment.analyze(status.text);
-        console.log("-- Sentiment --");
-        console.log(sentiment);
+          // check if the status already exists in the database
+          var isExist = _db_tasks.findOne({
+            id: status.id_str
+          });
 
-        _db_tasks.insert({
-          text: status.text,
-          sentiment: sentiment.score,
-          createdAt: status.created_at,
-          owner: status.user.name,
-          username: status.user.screen_name
-        });
-      };
-      
-    },
-    function(e) {
-      console.log('bind failure');
-    }
-  ));
+          // analyse sentiment and inset into db
+          if(isExist == null){
+            var sentiment = speakeasy.sentiment.analyze(status.text);
+            console.log("-- Sentiment --");
+            console.log(sentiment);
+
+            _db_tasks.insert({
+              id: status.id_str,
+              text: status.text,
+              sentiment: sentiment.score,
+              createdAt: status.created_at,
+              owner: status.user.name,
+              username: status.user.screen_name
+            });
+          }
+
+        };
+        
+      },
+      function(e) {
+        console.log('twitterget bind failure');
+      }
+    ));
+  },
+  function(e){
+    console.log('setInterval bind failure');
+  }), 5000);
 
 }
