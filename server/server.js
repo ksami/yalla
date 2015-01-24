d19a413462a73e56302752ad97db0a1a3a10976f
Thearith <sothearith.sreang@gmail.com>
2015-01-24 20:30:26 +0800

    nth

--- server/server.js
+++ server/server.js
@@ -11,28 +11,18 @@ if (Meteor.isServer) {
     });
   });
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 
-
-  var twitter = new TwitMaker(Meteor.__keys);
-  var speakeasy = Meteor.npmRequire('speakeasy-nlp');
-
   // async callbacks to be wrapped in Meteor.bindEnvironment()
-  twitter.get('search/tweets', { q: 'snsd since:2011-11-11 lang:en', count: 3 }, Meteor.bindEnvironment(
+  twitter.get('search/tweets', { q: 'banana since:2011-11-11 lang:en', count: 3 }, Meteor.bindEnvironment(
     function(err, data, response) {
       console.log(data);
 
       for (var i = data.statuses.length - 1; i >= 0; i--) {
         var status = data.statuses[i];
 
-        var sentiment = speakeasy.sentiment.analyze(status.text);
-        console.log("-- Sentiment --");
-        console.log(sentiment);
-
         _db_tasks.insert({
           text: status.text,
-          sentiment: sentiment.score,
           createdAt: status.created_at,
           owner: status.user.name,
           username: status.user.screen_name
