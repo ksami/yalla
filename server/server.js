930fea4fd87480c19af3462aba22a75f0573a167
Thearith <sothearith.sreang@gmail.com>
2015-01-24 19:35:47 +0800

    add lang filter

--- server/server.js
+++ server/server.js
@@ -17,25 +17,25 @@ if (Meteor.isServer) {
   var twitter = new TwitMaker(Meteor.__keys);
 
   // async callbacks to be wrapped in Meteor.bindEnvironment()
-  twitter.get('search/tweets', { q: 'banana since:2011-11-11', count: 3 }, Meteor.bindEnvironment(
-    function(err, data, response) {
-      console.log(data);
+    twitter.get('search/tweets', { q: 'banana since:2011-11-11', lang: en, count: 3 }, Meteor.bindEnvironment(
+      function(err, data, response) {
+        console.log(data);
 
-      for (var i = data.statuses.length - 1; i >= 0; i--) {
-        var status = data.statuses[i];
+        for (var i = data.statuses.length - 1; i >= 0; i--) {
+          var status = data.statuses[i];
 
-        _db_tasks.insert({
-          text: status.text,
-          createdAt: status.created_at,
-          owner: status.user.name,
-          username: status.user.screen_name
-        });
-      };
-      
-    },
-    function(e) {
-      console.log('bind failure');
-    }
-  ));
+          _db_tasks.insert({
+            text: status.text,
+            createdAt: status.created_at,
+            owner: status.user.name,
+            username: status.user.screen_name
+          });
+        };
+        
+      },
+      function(e) {
+        console.log('bind failure');
+      }
+    ));
 
 }
