715a9e8e132a0a7ce873acaadee1ef418b87b8e2
Thearith <sothearith.sreang@gmail.com>
2015-01-25 03:00:33 +0800

    fix merge index.html

--- server/server.js
+++ server/server.js
@@ -25,41 +25,43 @@ if (Meteor.isServer) {
 
   // access topic 1 tweets
   setInterval(Meteor.bindEnvironment(function(){
-    twitter.get('search/tweets', { q: topic_1 + ' since:2011-11-11 lang:en', count: 20 }, Meteor.bindEnvironment(
-      function(err, data, response) {
-        console.log(data);
+    // twitter.get('search/tweets', { q: topic_1 + ' since:2011-11-11 lang:en', count: 20 }, Meteor.bindEnvironment(
+    //   function(err, data, response) {
+    //     console.log(data);
 
-        for (var i = data.statuses.length - 1; i >= 0; i--) {
-          var status = data.statuses[i];
+    //     for (var i = data.statuses.length - 1; i >= 0; i--) {
+    //       var status = data.statuses[i];
 
-          // check if the status already exists in the database
-          var isExist = _db_tweets_1.findOne({
-            id: status.id_str
-          });
+    //       // check if the status already exists in the database
+    //       var isExist = _db_tweets_1.findOne({
+    //         id: status.id_str
+    //       });
 
-          // analyse sentiment and inset into db
-          if(isExist == null){
-            var sentiment = speakeasy.sentiment.analyze(status.text);
-            console.log("-- Sentiment --");
-            console.log(sentiment);
+    //       // analyse sentiment and inset into db
+    //       if(isExist == null){
+    //         var sentiment = speakeasy.sentiment.analyze(status.text);
+    //         console.log("-- Sentiment --");
+    //         console.log(sentiment);
 
-            _db_tweets_1.insert({
-              id: status.id_str,
-              text: status.text,
-              sentiment: sentiment.score,
-              createdAt: status.created_at,
-              owner: status.user.name,
-              username: status.user.screen_name
-            });
-          }
+    //         _db_tweets_1.insert({
+    //           id: status.id_str,
+    //           text: status.text,
+    //           sentiment: sentiment.score,
+    //           createdAt: status.created_at,
+    //           owner: status.user.name,
+    //           username: status.user.screen_name
+    //         });
+    //       }
 
-        }
+    //     }
         
-      },
-      function(e) {
-        console.log('twitterget 1 bind failure');
-      }
-    ));
+    //   },
+    //   function(e) {
+    //     console.log('twitterget 1 bind failure');
+    //   }
+    // ));
+      console.log("1 called " + Date.now());
+
   },
   function(e){
     console.log('setInterval 1 bind failure');
@@ -68,41 +70,42 @@ if (Meteor.isServer) {
   //access topic 2 tweets
   setTimeout(Meteor.bindEnvironment( function() {
       setInterval(Meteor.bindEnvironment(function(){
-      twitter.get('search/tweets', { q: topic_2 + ' since:2011-11-11 lang:en', count: 20 }, Meteor.bindEnvironment(
-        function(err, data, response) {
-          console.log(data);
+      // twitter.get('search/tweets', { q: topic_2 + ' since:2011-11-11 lang:en', count: 20 }, Meteor.bindEnvironment(
+      //   function(err, data, response) {
+      //     console.log(data);
 
-          for (var i = data.statuses.length - 1; i >= 0; i--) {
-            var status = data.statuses[i];
+      //     for (var i = data.statuses.length - 1; i >= 0; i--) {
+      //       var status = data.statuses[i];
 
-            // check if the status already exists in the database
-            var isExist = _db_tweets_2.findOne({
-              id: status.id_str
-            });
+      //       // check if the status already exists in the database
+      //       var isExist = _db_tweets_2.findOne({
+      //         id: status.id_str
+      //       });
 
-            // analyse sentiment and inset into db
-            if(isExist == null){
-              var sentiment = speakeasy.sentiment.analyze(status.text);
-              console.log("-- Sentiment --");
-              console.log(sentiment);
+      //       // analyse sentiment and inset into db
+      //       if(isExist == null){
+      //         var sentiment = speakeasy.sentiment.analyze(status.text);
+      //         console.log("-- Sentiment --");
+      //         console.log(sentiment);
 
-              _db_tweets_2.insert({
-                id: status.id_str,
-                text: status.text,
-                sentiment: sentiment.score,
-                createdAt: status.created_at,
-                owner: status.user.name,
-                username: status.user.screen_name
-              });
-            }
+      //         _db_tweets_2.insert({
+      //           id: status.id_str,
+      //           text: status.text,
+      //           sentiment: sentiment.score,
+      //           createdAt: status.created_at,
+      //           owner: status.user.name,
+      //           username: status.user.screen_name
+      //         });
+      //       }
 
-          }
+      //     }
           
-        },
-        function(e) {
-          console.log('twitterget 2 bind failure');
-        }
-      ));
+      //   },
+      //   function(e) {
+      //     console.log('twitterget 2 bind failure');
+      //   }
+      // ));
+        console.log("2 called " + Date.now());
     },
     function(e){
       console.log('setInterval 2 bind failure');
